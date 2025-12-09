3c0d39e0fcf4bd2fc6f03d9fe8c0e8afba7e68ff
Jeteish Pratap Singh <jsing162@asu.edu>
2025-12-09 10:11:58 -0700

    demo part added

--- src/trackerList.js
+++ src/trackerList.js
@@ -1,26 +1,23 @@
-// Basic list of known tracker domains for demonstration purposes
-const TRACKER_DOMAINS = [
+export const trackers = [
     "google-analytics.com",
+    "analytics.google.com",
     "doubleclick.net",
-    "facebook.com/tr",
-    "adservice.google.com",
     "googletagmanager.com",
-    "analytics.twitter.com",
-    "bat.bing.com"
+    "facebook.com",
+    "facebook.net",
+    "ads.yahoo.com",
+    "adservice.google.com",
+    "scorecardresearch.com",
+    "segment.io",
+    "hotjar.com",
+    "mixpanel.com"
 ];
 
-/**
- * Checks if a given URL belongs to a known tracker.
- * @param {string} url - The URL to check.
- * @returns {boolean} - True if it is a tracker, false otherwise.
- */
 export function isTracker(url) {
     try {
-        const urlObj = new URL(url);
-        const nonNullHost = urlObj.hostname || "";
-
-        return TRACKER_DOMAINS.some(domain => nonNullHost.includes(domain));
-    } catch (e) {
+        const host = new URL(url).hostname;
+        return trackers.some(t => host.includes(t));
+    } catch {
         return false;
     }
 }
